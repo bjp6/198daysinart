c3e5bb566db9dddc269f346286f9b641e5247064
bjp6 <78206856+bjp6@users.noreply.github.com>
2024-07-15 14:22:29 +0200

    with us1 trigger and echo

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ firmware_url = "https://raw.githubusercontent.com/bjp6/198daysinart/"
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
 
-"""trigger = Pin(3, Pin.OUT)
-echo = Pin(2, Pin.IN)"""
+us1_trigger = Pin(3, Pin.OUT)
+us1_echo = Pin(2, Pin.IN)
+us2_trigger = Pin(4, Pin.OUT)
+us2_echo = Pin(5, Pin.IN)
 
 sda = Pin(0)
 scl = Pin(1)
 id=0
 
+print("Pins defined")
+
 i2c = I2C(id=id, sda=sda, scl=scl)
+
+print("i2c adress accepted")
 
 pca = pca9685.PCA9685(i2c=i2c)
 servo = Servos(i2c=i2c)
@@ -56,6 +62,8 @@ def enter_zone():
     servo.position(index=6, degrees=0)
     servo.position(index=7, degrees=0)
     servo.position(index=8, degrees=0)
+
+    print("servos in position 0")
     
     """servo.position(index=0, degrees=0)
     servo.position(index=1, degrees=0)
@@ -87,53 +95,50 @@ def enter_zone():
     servo.position(index=7, degrees=0)
     servo.position(index=8, degrees=0)"""
     
-for i in range (0,180):
-    servo.position(index=0, degrees=i)
-    servo.position(index=1, degrees=i)
-    servo.position(index=2, degrees=i)
-    servo.position(index=3, degrees=i)
-    servo.position(index=4, degrees=i)
-    servo.position(index=5, degrees=i)
-    servo.position(index=6, degrees=i)
-    servo.position(index=7, degrees=i)
-    servo.position(index=8, degrees=i)
-    utime.sleep(0.030)   # move 1 degree and wait 20 milliseconds, relatively slow and smooth operation
+    for i in range (0,180):
+        servo.position(index=0, degrees=i)
+        servo.position(index=1, degrees=i)
+        servo.position(index=2, degrees=i)
+        servo.position(index=3, degrees=i)
+        servo.position(index=4, degrees=i)
+        servo.position(index=5, degrees=i)
+        servo.position(index=6, degrees=i)
+        servo.position(index=7, degrees=i)
+        servo.position(index=8, degrees=i)
+        utime.sleep(0.030)   # move 1 degree and wait 20 milliseconds, relatively slow and smooth operation
 
-utime.sleep(5)
+    utime.sleep(5)
 
-for i in range (180,-1, -1):
-    servo.position(index=0, degrees=i)
-    servo.position(index=1, degrees=i)
-    servo.position(index=2, degrees=i)
-    servo.position(index=3, degrees=i)
-    servo.position(index=4, degrees=i)
-    servo.position(index=5, degrees=i)
-    servo.position(index=6, degrees=i)
-    servo.position(index=7, degrees=i)
-    servo.position(index=8, degrees=i)
-    utime.sleep(0.030)   # move 1 degree and wait 20 milliseconds, relatively slow and smooth operation
-
-
-
+    for i in range (180,-1, -1):
+        servo.position(index=0, degrees=i)
+        servo.position(index=1, degrees=i)
+        servo.position(index=2, degrees=i)
+        servo.position(index=3, degrees=i)
+        servo.position(index=4, degrees=i)
+        servo.position(index=5, degrees=i)
+        servo.position(index=6, degrees=i)
+        servo.position(index=7, degrees=i)
+        servo.position(index=8, degrees=i)
+        utime.sleep(0.030)   # move 1 degree and wait 20 milliseconds, relatively slow and smooth operation
   
-"""def ultra():
-    trigger.low()
+def ultra():
+    us1_trigger.low()
     utime.sleep_us(2)
-    trigger.high()
+    us1_trigger.high()
     utime.sleep_us(5)
-    trigger.low()
-    while echo.value() == 0:
+    us1_trigger.low()
+    while us1_echo.value() == 0:
         signaloff = utime.ticks_us()
-    while echo.value() == 1:
+    while us1_echo.value() == 1:
         signalon = utime.ticks_us()
     timepassed = signalon - signaloff
     distance = (timepassed * 0.0343) / 2
     print(timepassed)
     print("The distance from object is ",distance,"cm")
-    if distance < 20:
+    if distance < 150:
         enter_zone()
-""" 
+
 while True:
-    enter_zone()#when ultrasonic sensor is installed this should read ultra()
+    ultra()#reads the ultrasonic sensors and starts functions accordingly
     utime.sleep(1)
 
